59d0792ec2946d8e288db8fe32915092e533cd86
Rémi Roycourt <rroycourt@Mac-mini-de-Remi.local>
2016-06-04 00:46:50 +0200

    Ajout firereact

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
 import '../assets/stylesheets/base.scss';
 import React, { Component } from 'react';
+import '../firebase-conf';
 
 const Hello = React.createClass({
+
+  mixins: [ReactFireMixin],
+
+  componentWillMount: function () {
+
+    var ref = firebase.database().ref("items");
+    this.bindAsArray(ref, "items");
+
+    this.firebaseRef.on("child_added", function (dataSnapshot) {
+
+      console.log(dataSnapshot.val());
+
+      this.items.push(dataSnapshot.val());
+      this.setState({
+        items: this.items
+      });
+    }.bind(this));
+
+  },
+
   render() {
-    return(
+    return (
       <h1>Hello test hackoffice, {this.props.name}!</h1>
     )
   }
---
+++ src/firebase-conf.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyAWzSSHA-lul3rUlxJ3OODnPIc6FlttBlQ",
+  authDomain: "project-3932329543505259132.firebaseapp.com",
+  databaseURL: "https://project-3932329543505259132.firebaseio.com",
+  storageBucket: "project-3932329543505259132.appspot.com",
+};
+
+firebase.initializeApp(config);
