93dc2ac0110132278be2300891ed76d551a78693
Christophe dumont <christophe@hooraylabs.com>
2016-06-04 00:32:50 +0200

    added good conf for webpack

--- server.js
+++ server.js
@@ -5,20 +5,14 @@ var PORT = process.env.PORT || 8080
 
 // using webpack-dev-server and middleware in development environment
 if(process.env.NODE_ENV !== 'production') {
+  var webpackDevMiddleware = require('webpack-dev-middleware');
+  var webpackHotMiddleware = require('webpack-hot-middleware');
   var webpack = require('webpack');
-  var WebpackDevServer = require('webpack-dev-server');
   var config = require('./webpack.config');
+  var compiler = webpack(config);
 
-  new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true
-  }).listen(3000, 'localhost', function (err, result) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log('Listening at http://localhost:3000/');
-  });
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+  app.use(webpackHotMiddleware(compiler));
 }
 
 app.use(express.static(path.join(__dirname, 'dist')));
