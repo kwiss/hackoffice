1dadf24633e323bf12ee25b0dde867e6e9265927
Rémi Roycourt <rroycourt@Mac-mini-de-Remi.local>
2016-06-04 22:55:06 +0200

    Modification Todo view

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -16,26 +16,12 @@ var Timeline = React.createClass({
         this.bindAsArray(this.firebaseRef, "todos");
     },
     createTimeline: function (item, index) {
-        switch (item.type) {
-            case 'todo':
-                var key = item[".key"];
-                return (<Todo key={index} id={key} />);
-            default:
-                return (<div key={index}>test</div>);
-        }
+        var key = item[".key"];
+        console.log(item);
+        return (<Todo key={index} id={key} />);
     },
     render: function () {
-
-        var _this = this;
-<<<<<<< HEAD
-        var createTimeline = function (item, index) {
-            var key = item[".key"];
-            return (<Todo key={index} id={key} />);
-        };
-        return <div>{this.state.todos.map(createTimeline) }</div>;
-=======
-        return <div className="timeline">{this.state.items.map(this.createTimeline) }</div>;
->>>>>>> 0ceb3fa133e83dc2d76c820ed2d985a795d22620
+        return <div>{this.state.todos.map(this.createTimeline) }</div>;
     }
 
 });
